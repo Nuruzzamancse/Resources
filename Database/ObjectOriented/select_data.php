e19718f9544e399c7557fe3e9e5477facdbf1ac0
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-02-16 18:16:25 +0600

    Database Resources

---
+++ Database/ObjectOriented/select_data.php
@@ -0,0 +1,58 @@
+
+
+</<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+
+    <style>
+    
+    table{
+        border:1px solid blue;
+    }
+    td{
+        border:1px solid red;
+    }
+
+    </style>
+
+
+</head>
+<body>
+
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "nkn";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+    
+</body>
+</html>
