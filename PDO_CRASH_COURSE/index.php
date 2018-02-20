e2a2b7fbef74a4860f5af363acce8e6f490644d7
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-02-20 22:13:16 +0600

    pdo crash course started

---
+++ PDO_CRASH_COURSE/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$dbname = 'blogpost';
+
+//Set DSN
+$dsn = 'mysql:host='.$host .';dbname='.$dbname;
+
+//Create a PDO instance
+
+
+
+?>
